7fe04d70045eff77b62a7d915ecbd7a89592f2b1
Rishi Joshi <joshirishi1912@gmail.com>
2025-03-27 18:02:34 +0530

    Add some new routes

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express=require('express');
 const app=express();    // create an instance of express
 const port=3000;        // port number  to connect to the server
 const path=require('path');  // path module to access the path of the file
+const methodoverride=require('method-override');
 
 app.use(express.urlencoded({extended:true}));  // to parse the data from the form
 app.use(express.json());  // to parse the data from the form
+app.use(methodoverride('_method'));  // to use the method override
 
 app.set('view engine','ejs');  // set the view engine to ejs
 app.set('views',path.join(__dirname,'views'));  // set the views directory to the views folder
@@ -21,15 +23,18 @@ app.get('/',(req,res)=>{
 let posts = [
     {
         username:"Rishi",
-        content:"This is my first post"
+        content:"This is my first post",
+        id:"1a"
     },
     {
         username:"Priya",
-        content:"This is my first internship"
+        content:"This is my first internship",
+        id:"2b"
     },
     {
         username:"Rajat",
-        content:"This is my first project"
+        content:"This is my first project",
+        id:"3c"
     }
 
 ];
@@ -45,5 +50,32 @@ app.get("/posts/new",(req,res)=>{
 app.post("/posts",(req,res)=>{
     let {username,content}=req.body;
     posts.push({username,content});
+    res.redirect('/posts');  // redirect to the posts page
+});
+
+app.get("/posts/:id",(req,res)=>{
+    let post=posts.find(p=>p.id===req.params.id);
+    if(!post) return res.sendStatus(404);  // if post not found send a 404 status
+    res.render('show.ejs',{post});  // render the show.ejs file
+});
+
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let post=posts.find(p=>p.id===req.params.id);
+    if(!post) return res.sendStatus(404);  // if post not found send a 404 status
+    res.render('edit.ejs',{post});  // render the edit.ejs file
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let post=posts.find(p=>p.id===req.params.id);
+    let {content}=req.body;
+    if(!post) return res.sendStatus(404);  // if post not found send a 404 status
+    post.content=content;
+    res.redirect('/posts');  // redirect to the posts page
+
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    posts=posts.filter(p=>p.id!==req.params.id);
     res.redirect('/posts');  // redirect to the posts page
 });
